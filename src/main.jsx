898d003e7f41c50b0dbf629a0fb6fbd009a398a7
alan kailani <alankailani3@gmail.com>
2025-07-08 18:03:51 -0400

    Fix Tailwind layout and center content

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import './styles.css';
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './App.css'; // Make sure this has the @tailwind directives
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
-)
+);
